249f4fae12e0124a177f02173ab4fc2c130efef5
Dschonas <jonasneumair@gmail.com>
2015-03-24 15:56:37 +0100

    slight changes in Bag.java

--- core/src/com/slot/Bag.java
+++ core/src/com/slot/Bag.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.slot;
 
 import javax.annotation.Resource;
@@ -45,3 +46,52 @@ public class Bag {
 	}
 		
 }
+=======
+package com.slot;
+
+import javax.annotation.Resource;
+
+/*
+ * author jonas
+ */
+
+public class Bag {
+
+	private Slot[] resource;
+	
+	public Bag() {
+		super();
+		resource = new Slot[12];
+	}
+	
+	
+	/*	
+	 * adds resources to a slot and 
+	 * returns the resources, which couldn't be added
+	 * 
+	 * checks if
+	 *  	you add the same resource
+	 *  	it has enough free space
+	 *  	if you add a valid resource
+	 */
+	public void add(Resource r, int plus) {
+			
+	}
+	
+	/*
+	 * deletes certain resource
+	 *
+	 * checks if
+	 * 		the SLOT contains the resource
+	 */
+	public void dispose(Resource r, int amount) {
+		
+	}
+		
+	public boolean clearSlot(int index) {
+		if(resource[index].isEmpty())
+		return false;
+	}
+		
+}
+>>>>>>> Stashed changes
