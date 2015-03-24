88d1d195710b3f9f0c248493ce78d2be363e5cda
astwys <astwys@gmail.com>
2015-03-24 19:25:24 +0100

    added decrease/increase health/hunger methods; added comments/better structure of the code

--- core/src/com/character/Character.java
+++ core/src/com/character/Character.java
@@ -12,30 +12,93 @@ import com.slot.Bag;
  */
 public class Character {
 	private String name;
-	private int hunger = 100, health = 100;
+	private int hunger = 100, health = 100; //default starting values
 	
 	private Bag bag;
 	private Craftingbook craftingbook;
 	
-	/**
-	 * 
-	 */
+	
+//---------------------------------------------------Constructor---------------------------------------------------
+
 	public Character(String name) {
 		setName(name);
 		bag=new Bag();
 		craftingbook=new Craftingbook();
 	}
 	
-	public int decreaseHunger() {
+	
+//---------------------------------------------------Hunger---------------------------------------------------
+/**
+ * hunger is inverted:
+ * 	decreaseHunger: character has more hunger
+ * 	increaseHunger: character has less hunger 
+ */
+	
+	/**
+	 * default reduction of hunger by 1
+	 * @return 
+	 */
+	public int decreaseHungerDef() {
 		setHunger(getHunger()-1);
 		return getHunger();
 	}
 	
-	public int decreaseHealth() {
-		setHealth(getHealth()-1);
+	/**
+	 * reduction of hunger by a certain number
+	 * @param dec (decrement)
+	 * @return
+	 */
+	public int decreaseHunger (int dec) {
+		setHunger(getHunger()-dec);
+		return getHunger();
+	}
+	
+	/**
+	 * increase hunger by a certain number
+	 * @param inc (increment)
+	 * @return
+	 */
+	public int increaseHunger (int inc) {
+		setHunger(getHunger()+inc);
 		return getHealth();
 	}
 	
+	
+//---------------------------------------------------Health---------------------------------------------------
+	
+	/**
+	 * default reduction of health by 1 of hunger is at 0
+	 * @return
+	 */
+	public int decreaseHealthDef() {
+		setHealth(getHealth()-1);
+		return getHealth();
+	} 
+	
+	/**
+	 * reduction of health by a certain number
+	 * @param dec (decrement)
+	 * @return
+	 */
+	public int decreaseHealth (int dec) {
+		setHealth(getHealth()-dec);
+		return getHealth();
+	}
+	
+	/**
+	 * increase health by a certain number
+	 * @param inc (increment)
+	 * @return
+	 */
+	public int increaseHealth (int inc) {
+		setHealth(getHealth()+inc);
+		return getHealth();
+	}
+	
+	
+//---------------------------------------------------Getters/Setters---------------------------------------------------
+	
+//-------------------------------Name-------------------------------
 	/**
 	 * @return the name
 	 */
@@ -49,11 +112,12 @@ public class Character {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	
+//-------------------------------Hunger-------------------------------
 	/**
-	 * @return the hunger
-	 * 
 	 * has to be between 100 (no hunger) and 1 (at 0 you loose live points)
+	 * @return the hunger
 	 */
 	public int getHunger() {
 		return hunger;
@@ -67,10 +131,13 @@ public class Character {
 			this.hunger = hunger;
 		}
 		else {
-			// method to decrease life
+			// method to decrease life if hunger = 0
+			// stop at 100 hunger
 		}
 	}
 	
+	
+//-------------------------------Health-------------------------------
 	/**
 	 * @return the health
 	 */
@@ -79,16 +146,16 @@ public class Character {
 	}
 	
 	/**
-	 * @param health the health to set
-	 * 
 	 * has to be between 100 (full health) and 1 (at 0 you die)
+	 * @param health the health to set
 	 */
 	public void setHealth(int health) {
 		if (health <=100 && health>0) {
 			this.health = health;
 		}
 		else {
-			// method to "die" / restart the game
+			// method to "die" / restart the game if health = 0
+			// stop at 100 health
 		}
 	}
 }
