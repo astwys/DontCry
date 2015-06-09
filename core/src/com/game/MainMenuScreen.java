2df4da2c6944cdd471ac6ff17fa836ba90f95a94
langhaarzombie <mixmax199989@googlemail.com>
2015-06-09 14:03:40 +0200

    added logo to the main menu

--- core/src/com/game/MainMenuScreen.java
+++ core/src/com/game/MainMenuScreen.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -20,6 +22,7 @@ public class MainMenuScreen implements Screen {
 	private Skin skin;
 	private Stage stage;
 	private TextButton txtbtn_play, txtbtn_options, txtbtn_exit;
+	private Image img_logo;
 	
 	public MainMenuScreen(final DontCry dontcry){
 		game = dontcry;
@@ -27,6 +30,12 @@ public class MainMenuScreen implements Screen {
 		stage = new Stage(new ExtendViewport(900, 600));
 		skin = new Skin(new FileHandle("../core/assets/skins/mainmenu/uiskin.json"));
 		initialiseButtons();
+		
+		img_logo = new Image(new Texture(new FileHandle("../core/assets/openingscreen/DontCryLogo.png")));
+		img_logo.setScale(0.35f);
+		img_logo.setPosition(280, 275);
+		stage.addActor(img_logo);
+		
 	}
 	
 	@Override
@@ -69,7 +78,7 @@ public class MainMenuScreen implements Screen {
 	public void initialiseButtons(){
 		//initialising txtbtn_play
 		txtbtn_play = new TextButton("Play", skin);
-		txtbtn_play.setBounds(900/2-80-80/2-40, 200, 80, 50);
+		txtbtn_play.setBounds(900/2-80-80/2-40, 170, 80, 50);
 		txtbtn_play.addListener(new ClickListener(){
 			
 			public void clicked(InputEvent input, float x, float y){
@@ -80,7 +89,7 @@ public class MainMenuScreen implements Screen {
 		
 		//initialise txtbtn_options
 		txtbtn_options = new TextButton("Options", skin);
-		txtbtn_options.setBounds(900/2-80/2, 200, 80, 50);
+		txtbtn_options.setBounds(900/2-80/2, 170, 80, 50);
 		txtbtn_options.addListener(new ClickListener(){
 			
 			public void clicked(InputEvent input, float x, float y){
@@ -90,7 +99,7 @@ public class MainMenuScreen implements Screen {
 		});
 		
 		txtbtn_exit = new TextButton("Exit", skin);
-		txtbtn_exit.setBounds(900/2+80/2+40, 200, 80, 50);
+		txtbtn_exit.setBounds(900/2+80/2+40, 170, 80, 50);
 		txtbtn_exit.addListener(new ClickListener(){
 			
 			public void clicked(InputEvent input, float x, float y){
